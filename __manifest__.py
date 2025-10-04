bd6f1e457c6ee56873deb56f94707805485d4add
ahmedmalsawi <81294781+ahmedmalsawi@users.noreply.github.com>
2025-10-04 22:51:17 +0300

    Create __manifest__.py

---
+++ __manifest__.py
@@ -0,0 +1,34 @@
+{
+    "name": "Journal Mass Operations",
+    "version": "1.0.0",
+    "category": "Accounting",
+    "summary": "Bulk operations for journal entries - Draft, Post, Cancel, Print",
+    "description": """
+        Bulk manage journal entries with one click
+        ==========================================
+        
+        Features:
+        • Mass Set to Draft - Convert posted entries to draft
+        • Mass Post Entries - Post multiple draft entries  
+        • Mass Cancel - Cancel multiple entries
+        • Mass Print - Generate PDF reports for multiple entries
+        
+        Compatible with Odoo 18 and 19.
+    """,
+    "author": "NEZAM ERP",
+    "website": "https://www.nezamerp.com",
+    "license": "LGPL-3",
+    "depends": ["account"],
+    "data": [
+        "views/account_move_views.xml",
+    ],
+    "images": [
+        "static/description/img/1.png", 
+        "static/description/img/nezamerp.png"  
+    ],
+    "installable": True,
+    "application": False,
+    "price": 1,
+    "currency": "USD",
+    "support": "admin@nezamerp.com",
+}
