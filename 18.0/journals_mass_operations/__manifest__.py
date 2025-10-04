ee85301c173c6edaac83b7c772b9c676af545369
ahmedmalsawi <81294781+ahmedmalsawi@users.noreply.github.com>
2025-10-04 23:19:18 +0300

    Update __manifest__.py

--- 18.0/journals_mass_operations/__manifest__.py
+++ 18.0/journals_mass_operations/__manifest__.py
@@ -1,15 +1,26 @@
 {
     "name": "JOURNAL MASS Operations",
-    "version": "18.0.1.0.0",
+    "version": "18.0.0.0.0",
     "depends": ["account"],
     "description": "Multiple mass operations for journal entries - Draft, Post, Cancel and more",
     "category": "Accounting",
     "summary": "Mass draft, post, cancel and print operations for journal entries",
     "author": "NEZAM ERP",
     "license": "LGPL-3",
-    "installable": True,
-    "application": False,
+        "author": "NEZAM ERP",
+    "website": "https://www.nezamerp.com",
+    "license": "OPL-1",
+    "depends": ["account"],
     "data": [
         "views/account_move_views.xml",
     ],
+    "images": [
+        "static/description/img/1.png", 
+        "static/description/img/nezamerp.png"  
+    ],
+    "installable": True,
+    "application": False,
+    "price": 1,
+    "currency": "USD",
+    "support": "admin@nezamerp.com",
 }
